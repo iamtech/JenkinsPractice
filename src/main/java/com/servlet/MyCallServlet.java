e8156f6662612242399752765d60de496d96396e
Amit Kumar Singh <amit.singh@tavant.com>
2018-09-14 17:44:46 +0530

    Changes Commited

--- src/main/java/com/servlet/MyCallServlet.java
+++ src/main/java/com/servlet/MyCallServlet.java
@@ -9,12 +9,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/MyCallServlet")
+@WebServlet("/MyCallServlet1")
 public class MyCallServlet extends HttpServlet{
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		req.setAttribute("msg", "Welcome");
+		req.setAttribute("msg", "Welcome Server 1");
 		 RequestDispatcher view = req.getRequestDispatcher("index.jsp");
 		 view.forward(req, resp);
     }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,7 +2,7 @@
 <body>
 <h2>Hello World!</h2>
 	<%  if(request.getAttribute("msg") == null) { %>
-	<form method="post" action="MyCallServlet">
+	<form method="post" action="MyCallServlet1">
 			<input type="submit" value="Get Message">
 	</form>
 	<% } %>
